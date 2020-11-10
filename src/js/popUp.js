7e4e1aea6d8d4184e4ec2e8221dfdaf035aff52f
Iryna Zalata <iryna.zalata@gmail.com>
2020-11-10 21:02:12 +0200

    Add "watch the trailer" option

--- src/js/popUp.js
+++ src/js/popUp.js
@@ -2,6 +2,7 @@ import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/src/styles/main.scss';
 
 import popUpTemplate from '../templates/popUp.hbs';
+import trailerTemplate from '../templates/trailer.hbs';
 import no_image_found from '../images/no-image.jpg';
 
 import '../css/popUp.css';
@@ -9,6 +10,12 @@ import '../css/popUp.css';
 const key = '401d61f37c17d956a98039a1a0734109';
 const showModal = async (id) => {
   const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${key}`);
+  const data = response.json();
+  return data
+}
+
+const showTrailer = async (query) => {
+  const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}%20official%20trailer&key=AIzaSyDigPVGWw58z95GRMpQLI5t-NGiAHpD3r0`);
   const data = response.json();
   return data
 }
@@ -26,7 +33,17 @@ document.querySelector('.home-film-list').addEventListener('click', (event) => {
       basicLightbox.create(`
     ${popUpTemplate(data)}
   `).show();
+
+    document.querySelector('.pop-up-btn-trailer').addEventListener('click', () => {
+    showTrailer(data.original_title)
+      .then(data =>
+        {basicLightbox.create(`
+    ${trailerTemplate(data.items[0])}
+  `).show();
+  })
 })
+    })
+  
 })
 
 window.addEventListener('keydown', (event) => {
